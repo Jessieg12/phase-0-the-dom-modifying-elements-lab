eb71c05787a242b5981160b75d3c832630fc7a4b
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-14 19:59:15 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removeMain = document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Jessie is the champion"
